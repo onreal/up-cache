09671e59c6c3a4e0016a50b088a2b6c127976151
Margarit Koka <panagiotis.kokas@gmail.com>
2022-07-20 23:22:45 +0300

    fix: optimize rules include and exclude

--- inc/Rules/UpCacheOptionsExclude.php
+++ inc/Rules/UpCacheOptionsExclude.php
@@ -6,27 +6,29 @@ use Upio\UpCache\Types\LifecycleTypes;
 use Upio\UpCache\UpCacheBase;
 
 class UpCacheOptionsExclude extends UpCacheBase implements IUpCacheRules {
-	public static function getType(): string {
-		return LifecycleTypes::Ignored;
-	}
+    public static function getType(): string {
+        return LifecycleTypes::Ignored;
+    }
 
-	public function setCss(): void {
-		$excluded = $this->getPluginOption( 'ignore_css_files_min' );
-		if ( !$excluded || empty( $excluded ) ) {
-			return;
-		}
-		self::setStyles( array( self::getType() => explode( ',', $excluded ) ) );
-	}
+    public function setCss(): void {
+        $excluded = $this->getPluginOption( 'ignore_css_files_min' );
+        if ( !$excluded || empty( $excluded ) ) {
+            return;
+        }
+        $excluded = explode( ',', $excluded );
+        self::setStyles( array( self::getType() =>  $excluded ) );
+    }
 
-	public function setJs(): void {
-		$excluded = $this->getPluginOption( 'ignore_js_files_min' );
-		if ( !$excluded || empty( $excluded ) ) {
-			return;
-		}
-		self::setStyles( array( self::getType() => explode( ',', $excluded ) ) );
-	}
+    public function setJs(): void {
+        $excluded = $this->getPluginOption( 'ignore_js_files_min' );
+        if ( !$excluded || empty( $excluded ) ) {
+            return;
+        }
+        $excluded = explode( ',', $excluded );
+        self::setStyles( array( self::getType() => $excluded ) );
+    }
 
-	public function setIntegrationName(): void {
-		self::setSupportName( 'up_cache_options' );
-	}
+    public function setIntegrationName(): void {
+        self::setSupportName( 'up_cache_options' );
+    }
 }
