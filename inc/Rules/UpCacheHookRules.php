041fd4764337eec019928e8dabcb2264f11f7bdd
Margarit Koka <panagiotis.kokas@gmail.com>
2022-07-22 12:39:06 +0300

    feat: new rule in order to add rules by filter hooks

---
+++ inc/Rules/UpCacheHookRules.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Upio\UpCache\Rules;
+
+use Upio\UpCache\Enums\LifecycleTypes;
+use Upio\UpCache\UpCacheBase;
+
+class UpCacheHookRules extends UpCacheBase implements IUpCacheRules
+{
+    public function setCss(): void
+    {
+        $rules = apply_filters('upio_up_cache_set_css_rules', array());
+        self::setStyles( $rules );
+    }
+
+    public function setJs(): void
+    {
+        $rules = apply_filters('upio_up_cache_set_js_rules', array());
+        self::setStyles( $rules );
+    }
+
+    public function setName(): void
+    {
+        self::setRuleName('hooked_rules');
+    }
+}
